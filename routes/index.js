127c36fe2bd59a543381bb25b4883b5737794c3e
nitingpt000 <nitingpt000@gmail.com>
2021-03-18 18:56:15 +0530

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+router.get("/__test", function (req, res, next) {
+  res.send("hello world");
+});
+
+module.exports = router;
