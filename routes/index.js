26878e3034d212badd4c108a7c66696e40dd607f
nitingpt000 <nitingpt000@gmail.com>
2021-03-21 16:00:55 +0530

    initial setup for api of the Vuyit ecommerce app

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.send("homepage");
 });
 router.get("/__test", function (req, res, next) {
   res.send("hello world");
